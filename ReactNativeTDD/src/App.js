088c645762a4c21358a8b8b2936cf523a98f056f
daejoon <kkode1911@gmail.com>
2022-07-09 13:08:57 +0900

    lecture/react-native-tdd

---
+++ ReactNativeTDD/src/App.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/fa cebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import AddToDo from './AddToDo';
+import ToDoList from './ToDoList';
+
+export default class App extends Component {
+    state = {
+        items: []
+    }
+
+    onAdded = (text) => {
+        this.setState({
+            items: [
+                {text, completed: false},
+                ...this.state.items
+            ]
+        })
+    }
+
+    onCompleted = (index) => {
+        this.setState({
+            items: this.state.items.map((item, i) => {
+                if ( index != i ) return item;
+
+                return {
+                    ...item,
+                    completed: !item.completed
+                };
+            })
+        })
+    }
+
+    onDeleted = (index) => {
+        this.setState({
+            items: [...this.state.items.slice(0, index), ...this.state.items.slice(index+1)]
+        })
+    }
+
+
+    render() {
+        const {items} = this.state;
+
+        return (
+            <View testID="welcome">
+                <Text>ToDo TDD</Text>
+                <AddToDo onAdded={this.onAdded}></AddToDo>
+                <ToDoList items={items} onCompleted={this.onCompleted} onDeleted={this.onDeleted}></ToDoList>
+            </View>
+        );
+    }
+};
---
+++ ReactNativeTDD/test/App.spec.js
@@ -0,0 +1,25 @@
+/**
+ * @format
+ */
+import 'react-native';
+import React from 'react';
+import {Text} from 'react-native'
+import {shallow} from 'enzyme';
+
+import App from '../src/App';
+
+describe('App', () => {
+    const wrapper = shallow(<App></App>);
+
+    it('is Text visible ?', () => {
+        expect(wrapper.find('Text').contains('ToDo TDD')).toBe(true);
+    });
+
+    it('is AddToDo visible ?', () => {
+        expect(wrapper.find('AddToDo')).toHaveLength(1);
+    });
+
+    it('is ToDoList visible ?', () => {
+        expect(wrapper.find('ToDoList')).toHaveLength(1);
+    });
+});
