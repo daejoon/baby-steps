dd8aca937ce05918d3ee08fb0e69552cf717e977
daejoon <kkode1911@gmail.com>
2022-08-21 14:56:38 +0900

    테스트 전부 코틀린으로 옮기기

---
+++ java-to-kotlin/src/test/kotlin/com/ddoong2/javatokotlin/JavaToKotlinApplicationTest.kt
@@ -0,0 +1,17 @@
+package com.ddoong2.javatokotlin
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class JavaToKotlinApplicationTest {
+
+
+    @Test
+    @DisplayName("Spring Boot 환경을 로드 합니다")
+    fun contextLoads() {
+
+    }
+
+}
