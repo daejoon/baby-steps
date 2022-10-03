27614054ca37aeb0df0c06a95e784f6210f1444e
daejoon <kkode1911@gmail.com>
2022-10-03 16:33:41 +0900

    initial commit

---
+++ web-app-on-spring-and-jpa/src/main/java/com/ddoong2/webapponspringandjpa/WebAppOnSpringAndJpaApplication.java
@@ -0,0 +1,13 @@
+package com.ddoong2.webapponspringandjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppOnSpringAndJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppOnSpringAndJpaApplication.class, args);
+    }
+
+}
