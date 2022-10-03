4d470e02a57ad9b0a8e09236bfe37ec16c8f84f3
daejoon <kkode1911@gmail.com>
2022-10-03 20:33:20 +0900

    회원가입 컨트롤러 구현

---
+++ web-app-on-spring-and-jpa/webapp/src/main/java/com/ddoong2/webapp/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.ddoong2.webapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /**
+     * WebSecurityConfigurerAdapter 가 @Deprecated 되어서 SecurityFilterChain 으로 대체
+     * 참고 : https://docs.spring.io/spring-security/reference/servlet/configuration/java.html#jc-httpsecurity
+     *
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .mvcMatchers("/",
+                        "/login",
+                        "/sign-up",
+                        "/check-email",
+                        "/check-email-token",
+                        "/email-login",
+                        "/check-email-login",
+                        "/login-link"
+                ).permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+
+}
---
+++ web-app-on-spring-and-jpa/webapp/src/test/java/com/ddoong2/webapp/account/AccountControllerTest.java
@@ -0,0 +1,39 @@
+package com.ddoong2.webapp.account;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    @DisplayName("회원 가입 화면 보이는지 테스트")
+    void 회원_가입_화면_보이는지_테스트() throws Exception {
+
+        // Given - 사전 조건 설정
+        final String url = "/sign-up";
+
+        // When - 검증하려는 로직 실행
+        final ResultActions actual = this.mockMvc.perform(get(url))
+                .andDo(print());
+
+        // Then - 출력 확인
+        actual.andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"));
+    }
+}
