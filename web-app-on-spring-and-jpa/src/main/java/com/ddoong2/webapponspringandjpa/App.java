d0d7dd6d8dc7e43340faf53809c85eb5bf711056
daejoon <kkode1911@gmail.com>
2022-10-03 17:25:39 +0900

    WebAppOnSpringAndJpaApplication 에서 App으로 Rename

--- web-app-on-spring-and-jpa/src/main/java/com/ddoong2/webapponspringandjpa/App.java
+++ web-app-on-spring-and-jpa/src/main/java/com/ddoong2/webapponspringandjpa/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebAppOnSpringAndJpaApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebAppOnSpringAndJpaApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
