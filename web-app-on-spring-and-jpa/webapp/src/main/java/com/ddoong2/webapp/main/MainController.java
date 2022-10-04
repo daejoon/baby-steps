c33555c68df5e75d59176657321c0ddc680f5158
daejoon <kkode1911@gmail.com>
2022-10-05 00:16:04 +0900

    현재 인증된 사용자 정보 참조
    - 여기서 가장 재미있는건 어노테이션에 어노테이션을 붙이면 상속과 같은 효과를 나타내는 것이다.

---
+++ web-app-on-spring-and-jpa/webapp/src/main/java/com/ddoong2/webapp/main/MainController.java
@@ -0,0 +1,20 @@
+package com.ddoong2.webapp.main;
+
+import com.ddoong2.webapp.account.CurrentUser;
+import com.ddoong2.webapp.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(@CurrentUser Account account, Model model) {
+
+        if (account != null) {
+            model.addAttribute(account);
+        }
+        return "index";
+    }
+}
