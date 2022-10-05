038e716268375fa2c2e53dee1df2dfc0aca53c63
daejoon <kkode1911@gmail.com>
2022-10-05 23:20:50 +0900

    프로필 수정 폼

---
+++ web-app-on-spring-and-jpa/webapp/src/main/java/com/ddoong2/webapp/settings/Profile.java
@@ -0,0 +1,20 @@
+package com.ddoong2.webapp.settings;
+
+import com.ddoong2.webapp.domain.Account;
+import lombok.Data;
+
+@Data
+public class Profile {
+
+    private String bio;
+    private String url;
+    private String occupation;
+    private String location;
+
+    public Profile(final Account account) {
+        this.bio = account.getBio();
+        this.url = account.getUrl();
+        this.occupation = account.getOccupation();
+        this.location = account.getLocation();
+    }
+}
