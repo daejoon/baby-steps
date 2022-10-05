a1dc46f09ae03722746438a9d6fed16b5d0fa436
daejoon <kkode1911@gmail.com>
2022-10-05 17:16:08 +0900

    프로필 뷰

--- web-app-on-spring-and-jpa/webapp/src/main/java/com/ddoong2/webapp/account/AccountController.java
+++ web-app-on-spring-and-jpa/webapp/src/main/java/com/ddoong2/webapp/account/AccountController.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Optional;
@@ -93,5 +90,17 @@ public class AccountController {
 
         accountService.sendSignUpConfirmEmail(account);
         return "redirect:/";
+    }
+
+    @GetMapping("/profile/{nickname}")
+    public String viewProfile(@PathVariable String nickname, Model model, @CurrentUser Account account) {
+
+        final Account findAccount = accountRepository.findByNickname(nickname)
+                .orElseThrow(() -> new IllegalArgumentException(nickname + "에 해당하는 사용자가 없습니다."));
+
+        model.addAttribute(findAccount);
+        model.addAttribute("isOwner", findAccount.equals(account));
+
+        return "account/profile";
     }
 }
