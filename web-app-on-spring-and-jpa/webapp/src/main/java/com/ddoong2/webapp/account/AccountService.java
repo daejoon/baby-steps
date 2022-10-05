c9c6924b3b01aeeddf9ce55f597a7ac540be6c93
daejoon <kkode1911@gmail.com>
2022-10-05 14:47:21 +0900

    로그인 로그 아웃
    - 강좌와 다르게 Optional의 or 메소드를 이용하면 소스가 가독성이 좋아진다고 생각한다.

--- web-app-on-spring-and-jpa/webapp/src/main/java/com/ddoong2/webapp/account/AccountService.java
+++ web-app-on-spring-and-jpa/webapp/src/main/java/com/ddoong2/webapp/account/AccountService.java
@@ -7,6 +7,9 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +18,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class AccountService {
+public class AccountService implements UserDetailsService {
 
     private final AccountRepository accountRepository;
     private final JavaMailSender javaMailSender;
@@ -64,4 +67,13 @@ public class AccountService {
         SecurityContextHolder.getContext().setAuthentication(token);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(final String emailOrNickname) throws UsernameNotFoundException {
+
+        final Account account = accountRepository.findByEmail(emailOrNickname)
+                .or(() -> accountRepository.findByNickname(emailOrNickname))
+                .orElseThrow(() -> new UsernameNotFoundException("이메일이나 닉네임으로 사용자를 찾을수 없습니다."));
+
+        return new UserAccount(account);
+    }
 }
--- web-app-on-spring-and-jpa/webapp/src/main/java/com/ddoong2/webapp/config/SecurityConfig.java
+++ web-app-on-spring-and-jpa/webapp/src/main/java/com/ddoong2/webapp/config/SecurityConfig.java
@@ -25,15 +25,15 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .mvcMatchers("/",
-                        "/login",
-                        "/sign-up",
-                        "/check-email-token",
-                        "/email-login",
-                        "/check-email-login",
-                        "/login-link").permitAll()
+                .mvcMatchers("/", "/login", "/sign-up", "/check-email-token", "/email-login", "/check-email-login", "/login-link").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated();
+
+        http.formLogin()
+                .loginPage("/login").permitAll();
+
+        http.logout()
+                .logoutSuccessUrl("/");
 
         return http.build();
     }
