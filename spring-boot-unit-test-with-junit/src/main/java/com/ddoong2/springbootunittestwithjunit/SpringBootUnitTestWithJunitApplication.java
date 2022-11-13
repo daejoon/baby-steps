ccaf19ea41adab0b069f472ea263365d0353d2f2
daejoon <kkode1911@gmail.com>
2022-11-13 13:22:33 +0900

    initial commit

---
+++ spring-boot-unit-test-with-junit/src/main/java/com/ddoong2/springbootunittestwithjunit/SpringBootUnitTestWithJunitApplication.java
@@ -0,0 +1,13 @@
+package com.ddoong2.springbootunittestwithjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnitTestWithJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUnitTestWithJunitApplication.class, args);
+	}
+
+}
---
+++ spring-boot-unit-test-with-junit/src/test/java/com/ddoong2/springbootunittestwithjunit/SpringBootUnitTestWithJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddoong2.springbootunittestwithjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitTestWithJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
