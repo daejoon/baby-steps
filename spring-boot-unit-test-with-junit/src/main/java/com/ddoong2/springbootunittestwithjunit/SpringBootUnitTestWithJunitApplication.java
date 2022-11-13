bc27eb20d451ab7a7c6edee12b78ff66e7d9206b
daejoon <kkode1911@gmail.com>
2022-11-13 13:37:05 +0900

    테스트 실패 하는것 수정

--- spring-boot-unit-test-with-junit/src/main/java/com/ddoong2/springbootunittestwithjunit/SpringBootUnitTestWithJunitApplication.java
+++ spring-boot-unit-test-with-junit/src/main/java/com/ddoong2/springbootunittestwithjunit/SpringBootUnitTestWithJunitApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootUnitTestWithJunitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootUnitTestWithJunitApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUnitTestWithJunitApplication.class, args);
+    }
 
 }
--- spring-boot-unit-test-with-junit/src/test/java/com/ddoong2/springbootunittestwithjunit/SpringBootUnitTestWithJunitApplicationTests.java
+++ spring-boot-unit-test-with-junit/src/test/java/com/ddoong2/springbootunittestwithjunit/SpringBootUnitTestWithJunitApplicationTests.java
@@ -1,13 +1,18 @@
 package com.ddoong2.springbootunittestwithjunit;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootUnitTestWithJunitApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    @DisplayName("환경을 로드 한다")
+    void 환경을_로드_한다() {
+        // Given - 사전 조건 설정
+        // When - 검증하려는 로직 실행
+        // Then - 출력 확인
+    }
 
 }
